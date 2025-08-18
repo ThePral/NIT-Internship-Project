8fb293c747ee37b3ccef834410d1fc2c4624fd55
AmirGhz17 <amirreza.ghazizade@gmail.com>
2025-08-18 21:06:12 +0330

    cors

--- backend/brilliant-talent-system/src/main.ts
+++ backend/brilliant-talent-system/src/main.ts
@@ -6,28 +6,38 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'PUT', 'POST', 'PATCH', 'OPTIONS'],
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Brilliant Talent System API')
     .setDescription('API documentation for authentication')
     .setVersion('1.0')
-    .addBearerAuth({
-      type: 'http',
-      scheme: 'bearer',
-      bearerFormat: 'JWT'
-    },
-    'access_token',
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access_token',
     )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
-  
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-    disableErrorMessages: false
-  }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      disableErrorMessages: false,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
